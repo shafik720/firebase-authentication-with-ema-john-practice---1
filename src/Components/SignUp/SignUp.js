309fe641d02578ec429b8ada4454bf42221580f1
shafik720 <shafikrasel5@gmail.com>
2022-11-15 23:44:02 +0600

    working on sign up  page

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -4,7 +4,33 @@ import './SignUp.css'
 const SignUp = () => {
     return (
         <div>
-            <h2>Sign Up</h2>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-lg-6 mx-auto">                        
+                        <div className=" form-div ">
+                        <h2>Log In</h2>
+                        <hr style={{marginBottom:"40px"}} />
+                            <form action="">
+                                <div className="email-field">
+                                    <p>Email :</p>
+                                    <input type="email" name="" id=""/>
+                                </div>
+                                <div className="password-field">
+                                    <p>Password :</p>
+                                    <input type="password" name="" id=""/>
+                                </div>
+                                <button>Login</button>
+                                <p className="signUpText">Dont Have an Account ? <Link to="/signup">Sign Up Here</Link> </p>
+                                <h4>Or</h4>
+                                <div draggable className="googleButton">
+                                    <img src={googleLogo} alt=""/>
+                                    <h4>Sign Using Google</h4>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
