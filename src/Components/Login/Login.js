fa97155471d273805f0afd00154ca9a165266685
shafik720 <shafikrasel5@gmail.com>
2022-11-15 23:10:43 +0600

    working on login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,7 +4,25 @@ import './Login.css'
 const Login = () => {
     return (
         <div>
-            <h2>Login</h2>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="">
+                            <form action="">
+                                <div className="email-field">
+                                    <p>Email :</p>
+                                    <input type="email" name="" id=""/>
+                                </div>
+                                <div className="password-field">
+                                    <p>Password :</p>
+                                    <input type="password" name="" id=""/>
+                                </div>
+                                <button>Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
